574d7b79f1329ce5622915f01d88cbcf7a966f8d
PETER MUYOMBA <111797748+pmuyomba11@users.noreply.github.com>
2023-08-01 18:42:00 -0400

    routes folder update

--- server.js
+++ server.js
@@ -3,11 +3,23 @@ require('dotenv').config();
 const app = express();
 const colors = require('colors');
 const PORT = process.env.PORT;
+const newsRouter = require('./src/routes/news');
 
-//Routers
-app.get('/', (req,res) => {
-    res.send("Hello World");
-})
+
+
+//Static files
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/imgs', express.static(__dirname + 'public/imgs'));
+app.use('/js', express.static(__dirname + 'public/js'));
+//Templating Engine...
+app.set("view engine", "ejs"); //setting ejs as view engine for rendering
+app.set('views', './src/views')
+
+
+//Middleware
+app.use('/news', newsRouter);
+
 
 
 //lISTENING....
