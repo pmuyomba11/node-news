68af522c8362e9d06839ef8a6cdf38def8007034
PETER MUYOMBA <111797748+pmuyomba11@users.noreply.github.com>
2023-08-01 22:59:46 -0400

    stles added

--- src/routes/news.js
+++ src/routes/news.js
@@ -6,7 +6,7 @@ router.get('/', async (req, res) => {
     // res.render('news')
 
     try {
-        const newsAPI = await axios.get(`https://newsapi.org/v2/everything?q=apple&from=2023-08-1&sortBy=popularity&apiKey=${process.env.API_KEY}`);
+        const newsAPI = await axios.get(`https://newsapi.org/v2/everything?q=apple&from=2023-07-29&sortBy=popularity&apiKey=${process.env.API_KEY}`);
         // console.log(newsAPI.data)
         res.render('news', {articles: newsAPI.data.articles})
     
